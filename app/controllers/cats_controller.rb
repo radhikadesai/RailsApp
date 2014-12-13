e3233f9b0d3f6557900bf56d379fe30d575d110c
radhika <rdesai@equalexperts.com>
2014-12-13 17:10:59 +0530

    added new and index features

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,8 +3,16 @@ class CatsController < ApplicationController
 		@ginger = p Cat.find params[:id]
 	end
 	def index
-		@cats = p Cat.all
+		@cats = Cat.all
 	end
 	def new
 	end
+	def create
+		cat = Cat.new
+		cat.name = params[:cat][:name]
+		cat.url = params[:cat][:url]
+		cat.save
+		redirect_to cat_path(cat)
+	end
+
 end
